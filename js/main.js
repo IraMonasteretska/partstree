5777763e13b2db3c1c8ef1ddb4a2b2a176b1296c
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-05-30 16:30:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // styled select
+    if ($('select').length) {
+        $('.styledselect').select2({
+            placeholder: "Select a state",
+            minimumResultsForSearch: Infinity,
+        });
+    }
+
+    // header
+    $('.header__acc').click(function(){
+        $('.header__accdd').toggleClass('show');
+    });
+
+
+
+
+
+
+
+
+
+});
