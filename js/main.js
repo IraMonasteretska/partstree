8a96f8df91ea7422b098cfe141e2c50c3ce49c21
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-06-06 15:58:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,28 +8,65 @@ $(document).ready(function () {
     }
 
     // header
-    $('.header__acc').click(function(){
+    $('.header__acc').click(function () {
         $('.header__accdd').toggleClass('show');
     });
 
-    $('.itembtn .close').click(function(){
+    $('.itembtn .close').click(function () {
         $(this).parent().remove();
     })
 
-    $('.showall').click(function(){
+    $('.showall').click(function () {
         $(this).parents('.filterbox').toggleClass('active');
         $(this).find('span').toggle();
     });
 
-    $('.filterbox .title').click(function(){
+    $('.filterbox .title').click(function () {
         $(this).next('.filterwrapbox').slideToggle();
     });
 
     // brands
-    $('.brandslist_more').click(function(){
+    $('.brandslist_more').click(function () {
         $(this).toggleClass('active');
         $(this).find('span').toggle();
         $(this).prev('.brandslist').toggleClass('open');
+    });
+
+
+    // product
+    if ($('.mySwiper').length) {
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesProgress: true,
+        });
+        var swiper2 = new Swiper(".mySwiper2", {
+            spaceBetween: 10,
+            // navigation: {
+            //   nextEl: ".swiper-button-next",
+            //   prevEl: ".swiper-button-prev",
+            // },
+            thumbs: {
+                swiper: swiper,
+            },
+        });
+    }
+
+    //   +-
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
     });
 
 
