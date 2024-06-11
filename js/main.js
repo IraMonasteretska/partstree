f3203780a3776ca8377e7e50014dd52648ff4b93
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-06-11 18:12:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -78,35 +78,186 @@ $(document).ready(function () {
 
 
 
-
-    if($(window).width() > 990) {
-        $(function () {
-            $('.imgZoom').mooZoom({
-                zoom: {
-                    width: 200,
-                    height: 200,
-                    zIndex: 600
-                },
-                overlay: {
-                    // opacity: 0.65,
-                    zIndex: 500,
-                    // backgroundColor: '#000000',
-                    fade: true
-                },
-                detail: {
-                    zIndex: 600,
-                    margin: {
-                        top: 0,
-                        left: 10
+    if ($('.imgZoom').length) {
+        if ($(window).width() > 990) {
+            $(function () {
+                $('.imgZoom').mooZoom({
+                    zoom: {
+                        width: 200,
+                        height: 200,
+                        zIndex: 600
                     },
-                    fade: true
-                },
-                animationDuration: 1000,
-    
-    
+                    overlay: {
+                        // opacity: 0.65,
+                        zIndex: 500,
+                        // backgroundColor: '#000000',
+                        fade: true
+                    },
+                    detail: {
+                        zIndex: 600,
+                        margin: {
+                            top: 0,
+                            left: 10
+                        },
+                        fade: true
+                    },
+                    animationDuration: 1000,
+
+
+                });
             });
-        });
+        }
     }
-   
+
+
+
+
+    var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom = document.getElementById("zoom");
+
+    function setTransform() {
+        zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+    }
+
+    zoom.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+    }
+
+    zoom.onmouseup = function (e) {
+        panning = false;
+    }
+
+    zoom.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+            return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+    }
+
+    zoom.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+            ys = (e.clientY - pointY) / scale,
+            delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+
+        setTransform();
+    }
+
+    // Обробники подій для кнопок zoom-in і zoom-out
+    document.getElementById("zoom-in").addEventListener("click", function () {
+        scale *= 1.2;
+        pointX = (pointX - zoom.clientWidth / 2) * 1.2 + zoom.clientWidth / 2;
+        pointY = (pointY - zoom.clientHeight / 2) * 1.2 + zoom.clientHeight / 2;
+        setTransform();
+    });
+
+    document.getElementById("zoom-out").addEventListener("click", function () {
+        scale /= 1.2;
+        pointX = (pointX - zoom.clientWidth / 2) / 1.2 + zoom.clientWidth / 2;
+        pointY = (pointY - zoom.clientHeight / 2) / 1.2 + zoom.clientHeight / 2;
+        setTransform();
+    });
+
+
+
+    // show modals
+    const schemeBoxes = document.querySelectorAll('.schemebox');
+
+    schemeBoxes.forEach(box => {
+        box.addEventListener('mouseover', function () {
+            const id = this.id;
+            const modal = document.querySelector(`.schmodal[data-modal="${id}"]`);
+            const circle = document.querySelector(`.circlesch[data-point="${id}"]`);
+            if (modal && circle) {
+                const circleRect = circle.getBoundingClientRect();
+                const zoomRect = document.getElementById('zoom').getBoundingClientRect();
+
+                modal.style.left = `${circleRect.left - zoomRect.left}px`;
+                modal.style.top = `${circleRect.top - zoomRect.top + circleRect.height}px`;
+                modal.style.display = 'block';
+            }
+        });
+
+        box.addEventListener('mouseout', function () {
+            const id = this.id;
+            const modal = document.querySelector(`.schmodal[data-modal="${id}"]`);
+            if (modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+
+
+
+
+
+
+    const circles = document.querySelectorAll('.circlesch');
+            
+    circles.forEach(circle => {
+        circle.addEventListener('click', function() {
+            const point = this.getAttribute('data-point');
+            const modal = document.querySelector(`.schmodal[data-modal="${point}"]`);
+            if (modal) {
+                if (modal.style.display === 'block') {
+                    modal.style.display = 'none';
+                } else {
+                    // Hide other modals
+                    document.querySelectorAll('.schmodal').forEach(m => {
+                        m.style.display = 'none';
+                    });
+
+                    const circleRect = this.getBoundingClientRect();
+                    const zoomRect = document.getElementById('zoom').getBoundingClientRect();
+                    const modalRect = modal.getBoundingClientRect();
+
+                    const leftPosition = circleRect.left - zoomRect.left + (circleRect.width / 2) - (modalRect.width / 2);
+                    const topPosition = circleRect.top - zoomRect.top + circleRect.height;
+
+                    modal.style.left = `${leftPosition}px`;
+                    modal.style.top = `${topPosition}px`;
+                    modal.style.display = 'block';
+                }
+            }
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!event.target.classList.contains('circlesch') && !event.target.closest('.schmodal')) {
+            const modals = document.querySelectorAll('.schmodal');
+            modals.forEach(modal => {
+                modal.style.display = 'none';
+            });
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
